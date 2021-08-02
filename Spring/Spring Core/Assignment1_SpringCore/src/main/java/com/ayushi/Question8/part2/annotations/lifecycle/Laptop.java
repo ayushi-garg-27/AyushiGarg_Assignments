1127f0b7f5ff674014c9bf0d6dd8a6495cd3111a
ayushi-garg-27 <87812785+ayushi-garg-27@users.noreply.github.com>
2021-08-02 18:19:05 +0530

    Add files via upload

---
+++ Spring/Spring Core/Assignment1_SpringCore/src/main/java/com/ayushi/Question8/part2/annotations/lifecycle/Laptop.java
@@ -0,0 +1,45 @@
+package com.ayushi.Question8.part2.annotations.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class Laptop {
+	private String colour;
+	private float price;
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		System.out.println("Selecting the Colour of Laptop");
+		this.colour = colour;
+	}
+	public float getPrice() {
+		System.out.println("Price of Laptop");
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public Laptop() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Shirt [colour=" + colour + ", price=" + price + "]";
+	} 
+	
+	@PostConstruct
+	public void start() {
+		System.out.println("Inside init method");
+	}
+	
+	@PreDestroy
+	public void end() {
+		System.out.println("Inside Destroy method");
+	}
+	
+
+}
+
+
