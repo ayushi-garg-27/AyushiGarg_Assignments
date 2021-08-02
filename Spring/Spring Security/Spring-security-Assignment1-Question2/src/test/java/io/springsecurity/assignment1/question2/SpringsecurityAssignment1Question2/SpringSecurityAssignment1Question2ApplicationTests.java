d2266994aa7ffbcbdf7a67b737549bc5e50ae273
ayushi-garg-27 <87812785+ayushi-garg-27@users.noreply.github.com>
2021-08-02 13:06:00 +0530

    Add files via upload

---
+++ Spring/Spring Security/Spring-security-Assignment1-Question2/src/test/java/io/springsecurity/assignment1/question2/SpringsecurityAssignment1Question2/SpringSecurityAssignment1Question2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springsecurity.assignment1.question2.SpringsecurityAssignment1Question2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAssignment1Question2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
